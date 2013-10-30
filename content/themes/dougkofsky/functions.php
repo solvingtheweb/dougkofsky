359fdf8b04217171193ab1964060d469121f9188
Chris Macholz <chris@solvingtheweb.com>
2013-10-30 08:50:16 -0400

    Added woocommerce support fix to follow existing template markup.

--- content/themes/dougkofsky/functions.php
+++ content/themes/dougkofsky/functions.php
@@ -110,6 +110,32 @@ function dougkofsky_scripts() {
 add_action( 'wp_enqueue_scripts', 'dougkofsky_scripts' );
 
 /**
+ * WooCommerce Compatibility Fix
+ */
+
+	// Remove default woocommerce content wrapper
+	remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+	remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+	
+	// Add in wrapper based on theme templates
+	add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+	add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+	function my_theme_wrapper_start() {
+	  echo '<div id="primary" class="content-area row">';
+	  echo '<main id="main" class="site-main" role="main">';
+	}
+
+	function my_theme_wrapper_end() {
+	  echo '</div>';
+	  echo '</main>';
+	}
+	
+	// Declare theme support for Woo Commerce
+	add_theme_support( 'woocommerce' );
+
+
+/**
  * Implement the Custom Header feature.
  */
 //require get_template_directory() . '/inc/custom-header.php';
