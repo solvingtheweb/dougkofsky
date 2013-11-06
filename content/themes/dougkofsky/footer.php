eb85c64480e26c0ff23bb0d5bcd5cc06d6a467e3
Chris Macholz <chris@solvingtheweb.com>
2013-11-05 22:46:43 -0500

    Removed unneeded content from footer

--- content/themes/dougkofsky/footer.php
+++ content/themes/dougkofsky/footer.php
@@ -12,10 +12,7 @@
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( 'dougkofsky_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'dougkofsky' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'dougkofsky' ), 'dougkofsky', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			&copy;<?php the_time('Y'); ?> Doug Kofsky
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
