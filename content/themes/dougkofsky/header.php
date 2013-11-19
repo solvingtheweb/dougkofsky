83823a0219c334cc0a6d16dc94819e8e764bc89d
Chris Macholz <chris@solvingtheweb.com>
2013-11-19 08:25:45 -0500

    Styled mini-cart and added correct links to cart/checkout.

--- content/themes/dougkofsky/header.php
+++ content/themes/dougkofsky/header.php
@@ -59,9 +59,10 @@
 	<div class="mini-cart-container">
 		<div class="mini-cart row">
 			<div class="large-12 columns">
-				<a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"> Your Cart</a>
-				<span><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> | </span>
-				<a href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="Checkout">Checkout</a>
+				<img src="<?php bloginfo('template_url'); ?>/images/cart.png">
+				<a class="cart-contents" href="<?php echo $woocommerce->cart->get_cart_url(); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>">Your Cart</a>
+				<span> - <?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count); ?> <span class="cart-pipe">|</span> </span>
+				<a href="<?php echo $woocommerce->cart->get_checkout_url(); ?>" title="Checkout">Checkout</a>
 			</div>
 		</div>
 	</div>
