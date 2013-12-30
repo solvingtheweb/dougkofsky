39d726ddfb4b96bde58f21451dc8a9b8741e4ef3
Chris Macholz <chris@solvingtheweb.com>
2013-12-29 20:55:33 -0500

    Added logic for options page social links to replace hard coded social link images on single product template

--- content/themes/dougkofsky/woocommerce/content-single-product.php
+++ content/themes/dougkofsky/woocommerce/content-single-product.php
@@ -61,9 +61,15 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 						<hr>
 						<div class="social">
 							<h4>Share this:</h4>
-    	
-							<a href="http://www.twitter.com/home?status=Doug%20Kofsky%20Photography%20-%20<?php the_permalink(); ?>" title="Share on Twitter" onclick="window.open(this.href, 'mywin','left=200,top=60,width=500,height=500,toolbar=1,resizable=1'); return false;"><img src="<?php bloginfo('template_directory'); ?>/images/twitter_button2.png" alt="Share on Twitter"></a>
-							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="Share on Facebook" target="_blank" onclick="window.open(this.href, 'mywin','left=200,top=60,width=500,height=500,toolbar=1,resizable=1'); return false;"><img src="<?php bloginfo('template_directory'); ?>/images/facebook_button2.png" alt="Share on Facebook"></a>
+    						<?php if(get_field('social_links', 'option')): ?>
+								<?php while(has_sub_field('social_links', 'option')): ?>
+									<?php if(get_sub_field('social_network_name') === "Twitter") : ?>						
+									<a href="http://www.twitter.com/home?status=Doug%20Kofsky%20Photography%20-%20<?php the_permalink(); ?>" title="Share on Twitter" onclick="window.open(this.href, 'mywin','left=200,top=60,width=500,height=500,toolbar=1,resizable=1'); return false;"><img src="<?php the_sub_field('social_network_icon');?>" alt="Share on Twitter"></a>
+									<?php elseif(get_sub_field('social_network_name') === "Facebook") : ?>
+									<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" title="Share on Facebook" target="_blank" onclick="window.open(this.href, 'mywin','left=200,top=60,width=500,height=500,toolbar=1,resizable=1'); return false;"><img src="<?php the_sub_field('social_network_icon');?>" alt="Share on Facebook"></a>
+									<?php endif; ?>
+								<?php endwhile; ?>
+							<?php endif; ?>
 							<?php if(get_field('flickr_link')) : ?>  
 						    	<a href="<?php the_field('flickr_link'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/flickr_button2.png" alt="View on Flickr"></a>
 							<?php endif; ?>
