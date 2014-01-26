8a30f2bc3d79189722d6ddc2999bde5b56c87d3b
Chris Macholz <chris@solvingtheweb.com>
2014-01-26 16:31:20 -0500

    Added favicon

--- content/themes/dougkofsky/header.php
+++ content/themes/dougkofsky/header.php
@@ -14,6 +14,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.ico" />
 <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,300,800,700' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
