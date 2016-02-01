d7ac7973df80ff5daa83c5febc36faf24ace55ab
Tim Macholz <tim@solvingtheweb.com>
2016-01-31 22:15:01 -0500

    updates to theme

--- content/themes/dougkofsky/image.php
+++ content/themes/dougkofsky/image.php
@@ -17,22 +17,23 @@ get_header(); ?>
 				<div class="entry-content full-image">
 					<div class="entry-attachment">
 						<div class="attachment">
-							<div class="mountainblock"></div>
-							<?php dougkofsky_the_attached_image(); ?>
-							<script type="text/javascript">
-								jQuery( document ).ready(function( $ ) {					
-									var img = $(".attachment-full"); // Get img elem
-									var pic_real_width, pic_real_height;
-									$("<img/>") // Make in memory copy of image to avoid css issues
-									    .attr("src", $(img).attr("src"))
-									    .load(function() {
-									        pic_real_width = this.width;   // Note: $(this).width() will not
-									        pic_real_height = this.height; // work for in memory images.
-											$(".mountainblock").css({"width": + pic_real_width, "height": + pic_real_height});
-									    });
-								});
-							</script>
-							
+							<a href="<?php echo get_permalink($post->post_parent); ?>" title="Back to Photo Details">
+								<div class="mountainblock"></div>
+								<?php dougkofsky_the_attached_image(); ?>
+								<script type="text/javascript">
+									jQuery( document ).ready(function( $ ) {					
+										var img = $(".attachment-full"); // Get img elem
+										var pic_real_width, pic_real_height;
+										$("<img/>") // Make in memory copy of image to avoid css issues
+										    .attr("src", $(img).attr("src"))
+										    .load(function() {
+										        pic_real_width = this.width;   // Note: $(this).width() will not
+										        pic_real_height = this.height; // work for in memory images.
+												$(".mountainblock").css({"width": + pic_real_width, "height": + pic_real_height});
+										    });
+									});
+								</script>
+							</a>
 						</div><!-- .attachment -->
 					</div><!-- .entry-attachment -->
 					<nav role="navigation" id="image-navigation" class="image-navigation">
